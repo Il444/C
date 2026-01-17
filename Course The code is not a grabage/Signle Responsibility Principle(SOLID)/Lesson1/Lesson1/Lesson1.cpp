69e5e5aa1cfbd470b98e92401e09983d83d5aa29
Il444 <ifedorenko08@icloud.com>
2026-01-17 19:29:44 +0300

    SOLID lives here

---
+++ Course The code is not a grabage/Signle Responsibility Principle(SOLID)/Lesson1/Lesson1/Lesson1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+int reverseNumbers(int number) {
+    int reversed = 0;
+    while (number > 0) {
+        reversed = reversed * 10 + (number % 10);
+        number /= 10;
+    }
+    return reversed;
+}
+
+bool isPrimes(int number) {
+    for (int divisor = 2; divisor < number - 1; divisor++) {
+        if (number % divisor == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isSuperPrimes(int number) {
+    return isPrimes(number) && isPrimes(reverseNumbers(number));
+}
+
+void printIfSuperPrimes(int number) {
+    if (isSuperPrimes(number)) {
+        cout << number << endl;
+    }
+}
+
+void processRange(int from, int to) {
+    for (int number = from; number <= to; number++) {
+        printIfSuperPrimes(number);
+    }
+}
+
+int main()
+{
+    cout << "Here are all simple numbers from 1 000 to 9 999 which satisfy the condition: " << endl;
+
+    processRange(1000, 9999);
+    return 0;
+}
