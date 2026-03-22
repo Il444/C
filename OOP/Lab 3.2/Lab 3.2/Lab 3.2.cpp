0c2965bee06b0a7a5d930082c627ddbacd47d840
Il444 <ifedorenko08@icloud.com>
2026-03-22 22:43:35 +0300

    Binary version released

--- OOP/Lab 3.2/Lab 3.2/Lab 3.2.cpp
+++ OOP/Lab 3.2/Lab 3.2/Lab 3.2.cpp
@@ -5,7 +5,8 @@
 using namespace std;
 
 int create(char* filename, long long count);
-int find_common(char* f, char* g, char* result);
+void read_file(char* filename);
+int find_common(char* f, char* g);
 
 int main() {
     srand((unsigned)time(NULL));
@@ -14,20 +15,26 @@ int main() {
     cout << "Enter number of elements in each file: ";
     cin >> n;
 
-    char f[40] = "f.txt";
-    char g[40] = "g.txt";
-    char result[40] = "result.txt";
+    char f[40] = "f.dat";
+    char g[40] = "g.dat";
 
     create(f, n);
     create(g, n);
 
-    find_common(f, g, result);
+    cout << "File f: ";
+    read_file(f);
+
+    cout << "File g: ";
+    read_file(g);
+
+    cout << "Common numbers: ";
+    find_common(f, g);
 
     return 0;
 }
 
 int create(char* filename, long long count) {
-    ofstream outfile(filename);
+    ofstream outfile(filename, ios::binary);
     if (!outfile) {
         cout << "Error: cannot create file" << endl;
         return 1;
@@ -36,61 +43,73 @@ int create(char* filename, long long count) {
     int current = 0;
     for (long long i = 0; i < count; i++) {
         current += rand() % 100 + 1;
-        outfile << current << " ";
+        outfile.write(reinterpret_cast<char*>(&current), sizeof(int));
     }
 
     outfile.close();
     return 0;
 }
 
-int find_common(char* f, char* g, char* result) {
-    ifstream file_f(f);
+void read_file(char* filename) {
+    ifstream infile(filename, ios::binary);
+    if (!infile) {
+        cout << "Error: cannot open file" << endl;
+        return;
+    }
+
+    int v;
+    int count = 0;
+    infile.read(reinterpret_cast<char*>(&v), sizeof(int));
+    while (!infile.eof() && count < 20) {
+        cout << v << " ";
+        count++;
+        infile.read(reinterpret_cast<char*>(&v), sizeof(int));
+    }
+    cout << endl;
+
+    infile.close();
+}
+
+int find_common(char* f, char* g) {
+    ifstream file_f(f, ios::binary);
     if (!file_f) {
         cout << "Error: cannot open file f" << endl;
         return 1;
     }
 
-    ifstream file_g(g);
+    ifstream file_g(g, ios::binary);
     if (!file_g) {
         cout << "Error: cannot open file g" << endl;
-        return 1;
-    }
-
-    ofstream outfile(result);
-    if (!outfile) {
-        cout << "Error: cannot create result file" << endl;
         return 1;
     }
 
     int val_f, val_g;
     bool found = false;
 
-    file_f >> val_f;
-    file_g >> val_g;
+    file_f.read(reinterpret_cast<char*>(&val_f), sizeof(int));
+    file_g.read(reinterpret_cast<char*>(&val_g), sizeof(int));
 
     while (!file_f.eof() && !file_g.eof()) {
         if (val_f == val_g) {
-            outfile << val_f << " ";
+            cout << val_f << " ";
             found = true;
-            file_f >> val_f;
-            file_g >> val_g;
+            file_f.read(reinterpret_cast<char*>(&val_f), sizeof(int));
+            file_g.read(reinterpret_cast<char*>(&val_g), sizeof(int));
         }
         else if (val_f < val_g) {
-            file_f >> val_f;
+            file_f.read(reinterpret_cast<char*>(&val_f), sizeof(int));
         }
         else {
-            file_g >> val_g;
+            file_g.read(reinterpret_cast<char*>(&val_g), sizeof(int));
         }
     }
 
     if (!found) {
-        outfile << "none";
+        cout << "none";
     }
-
-    cout << "Common numbers saved to result.txt" << endl;
+    cout << endl;
 
     file_f.close();
     file_g.close();
-    outfile.close();
     return 0;
 }
